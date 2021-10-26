f5f12a9120fad4e07df51976287901c41deabc7d
zeeshangengar1 <57321494+zeeshangengar1@users.noreply.github.com>
2021-10-26 12:40:08 +0530

    Add files via upload
    Cpp code for an algorithm

---
+++ algo1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main(){
+      int n;
+      cin>>n;
+     
+            
+
+    //   int xorsum=0;
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         xorsum=xorsum^nums[i];
+    //     }
+    //     int setBit=0;
+    //     int pos=0,temxor=xorsum,newxor=0;
+    //     while(setBit!=1)
+    //     {
+    //         setBit=xorsum&1;
+    //         pos++;
+    //         xorsum=xorsum>>1;
+    //     }
+    //      for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(nums[i]&(1<<pos))
+    //         {
+    //             newxor^=nums[i];
+    //         }
+    //     }
+    //     int k=newxor^tempxor;
+    //     vector<int> a[2];
+    //     a[0].push_back(newxor);
+    //     a[1].push_back(k);
+    //     return a;
+}
